f7dc8ba2fca129cb88990236dac41dc6f3f1013b
rifkiahmadfahrezi <rifkiaf589@gmail.com>
2023-09-17 09:57:10 +0700

    add popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
+// navbar menu toggler
 const menuToggler = document.querySelector(".menu-toggler");
 
 menuToggler.addEventListener("click", () => {
 	const navItems = document.querySelector(".navbar-items");
 	navItems.classList.toggle("show")
+})
+
+// popup toggler
+const popupTrigger = document.querySelector("[data-trigger-popup]")
+const closePopupBtn = document.querySelector("#close-popup")
+const popup = document.querySelector(".bg-popup")
+
+popupTrigger.addEventListener("click", () => {
+	popup.classList.toggle("show")
+})
+
+closePopupBtn.addEventListener("click", () => {
+	popup.classList.remove("show")
 })
